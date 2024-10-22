c3f5c43cf42ae737560f3ff3a77491c7cbd7dd76
Zahin_Abdullah <zahinabdullahrakin@gmail.com>
2024-10-23 01:41:20 +0600

    added the favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root/>
+  }
+], { basename:"/zahinabdullah/" })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
